de3e6b1661b13e0b676c7997c9429d861f14cf25
alan.ferreira@t2mlab.com <alan.ferreira@t2mlab.com>
2021-06-11 14:53:04 -0300

    Realiza upgrades

--- src/store/index.js
+++ src/store/index.js
@@ -12,11 +12,19 @@ export default createStore({
         ADD_GIFS(state, gifs){
             gifs.forEach(element => {
                 state.gifs.push(element)
+                console.log(state.gifs.length,'ADD_GIFS')
             });
             console.log(state.gifs.length)
         },
+        ADD_GIFS_RANDOM(state, gifs){
+                state.gifs.push(gifs)
+        },
         ADD_GIFS_SEARCH(state, gifs){
             state.gifs.unshift(gifs)
+        },
+        ADD_GIF_LIKE(state, gifs){
+            state.curtidos.push(gifs)
+            console.log(state.curtidos.length)
         }
     },
     actions: {
@@ -43,12 +51,34 @@ export default createStore({
             }
 
         },
-        async addMoreGifs({commit}, gifs){
-        //    console.log('addModeGifis no store', await gifs)
-                commit("ADD_GIFS", await gifs)
+        async moreGifsRandom(context){
+            try {
+                let url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.VUE_APP_API_KEY}&tag=&rating=g`
+
+                for(let i =0; i<5; i++){
+
+                    const req = await fetch(url)
+                    const res = await req.json()
+       
+                    if (res.meta.status == 200) {
+                        context.commit('ADD_GIFS_RANDOM', res.data)
+                        return res.data
+                    
+                    } else {
+                        context.commit('SET_GIFS', [])
+                        return []
+                    }
+                }
+            } catch (error) {
+                console.error(error)
+                throw error;
+            }
         },
         async addGifSearch({commit}, gifs){
             commit("ADD_GIFS_SEARCH", await gifs)
+        },
+        addGifLike(context){
+            this.commit("ADD_GIF_LIKE", context)
         }
     },
     getters: {
